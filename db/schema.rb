3d5e269a6e04820d4b5cb6d629a1562b799654e1
sasakama1984 <ideal_gate@icloud.com>
2020-02-13 09:43:21 +0000

    [Change]projectのカラムを変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_13_093248) do
+ActiveRecord::Schema.define(version: 2020_02_13_094208) do
 
   create_table "creaters", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -71,6 +71,8 @@ ActiveRecord::Schema.define(version: 2020_02_13_093248) do
     t.integer "reward", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "offer_id"
+    t.integer "request_id"
   end
 
   create_table "requests", force: :cascade do |t|
