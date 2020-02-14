442e588347d3d1e48621a4605df8daf5375e04ae
sasakama1984 <ideal_gate@icloud.com>
2020-02-14 21:22:56 +0000

    [Change]申請先名変更

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,2 +1,9 @@
 class Request < ApplicationRecord
+	has_one :project
+	enum request_status: {申請中:0, 契約済:1}
+	belongs_to :offer
+	belongs_to :creater
+	validates :opinion, presence: true
+	validates :hope_salary, presence: true
+	validates :production_span, presence: true
 end
