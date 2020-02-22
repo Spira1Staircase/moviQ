323d96e8a085c63294b595f5833cf710ad5056d9
sasakama1984 <ideal_gate@icloud.com>
2020-02-22 14:19:19 +0000

    [Add]aboutページを追加しました

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,23 @@
 //
 //= require rails-ujs
 //= require activestorage
+//= require jquery
 //= require bootstrap
 //= require_tree .
+//
+$(document).ready(function() {
+
+    $('.tab-content>div').hide();
+    $('.tab-content>div').first().slideDown();
+    $('.tab-buttons span').click(function() {
+        var thisclass = $(this).attr('class');
+        $('#lamp').removeClass().addClass('#lamp').addClass(thisclass);
+        $('.tab-content>div').each(function() {
+            if ($(this).hasClass(thisclass)) {
+                $(this).fadeIn(800);
+            } else {
+                $(this).hide();
+            }
+        });
+    });
+});
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,9 @@
 class ApplicationController < ActionController::Base
 	def after_sign_in_path_for(resources)
+		flash[:notice] = "ログインしました"
 		if creater_signed_in?
-			flash[:notice] = "ログインしました"
 			creater_path(current_creater)
 		else
-			flash[:notice] = "ログインしました"
 			employer_path(current_employer)
 		end
 	end
